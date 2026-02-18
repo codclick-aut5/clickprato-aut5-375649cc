2eab7d6b97c5654cc2962d4b7754dacb3ca8e775
codclick-aut5 <codclick.aut5@gmail.com>
2026-02-17 21:33:59 -0300

    Change Supabase URL and publishable key
    Updated Supabase URL and publishable key for the client.

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://ypsfoumafbfgcvrxylmw.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlwc2ZvdW1hZmJmZ2N2cnh5bG13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3NjkwODQsImV4cCI6MjA2MDM0NTA4NH0.aYVYGIcTbPeYONtCQaO543wfLstNDGBiUw-dUbNf6Xs";
+const SUPABASE_URL = "https://jjvtjuemmkmwkoxfzyng.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_hQJ92G4G60JPj1HBNRUR0A_5aa1zszf";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
